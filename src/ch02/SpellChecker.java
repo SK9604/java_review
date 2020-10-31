87faefa075335bad69e13bc8c43b1f196b897c28
SK9604 <tjsrud428@gmail.com>
2020-10-31 15:36:07 +0900

    SpellChecker

---
+++ src/ch02/SpellChecker.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class SpellChecker {
+	private static final Lexicon dictionary = ;
+	
+	private SpellChecker() {} //按眉 积己 规瘤
+	
+	public static boolean isValid(String word) {}
+	public static List<String> suggestions(String typo) {}
+}
