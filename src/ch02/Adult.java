f7f388ad6c95724527422ab15a18342701dd7d57
SK9604 <tjsrud428@gmail.com>
2020-11-07 19:11:06 +0900

    cleaner

---
+++ src/ch02/Adult.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Adult {
+	public static void main(String[] args) {
+		try(Room myRoom = new Room(7)) {
+			System.out.println("¾È³ç~");
+		}
+	}
+}
---
+++ src/ch02/Teenager.java
@@ -0,0 +1,8 @@
+package ch02;
+
+public class Teenager {
+	public static void main(String[] args) {
+		new Room(99);
+		System.out.println("¾Æ¹«·Å");
+	}
+}
