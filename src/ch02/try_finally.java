9c927f4482c54972e198a9792d3302dd91a2eaa0
SK9604 <tjsrud428@gmail.com>
2020-11-10 22:16:00 +0900

    지저분한 try-finally 방식

--- src/ch02/try_finally.java
+++ src/ch02/try_finally.java
@@ -1,8 +1,12 @@
 package ch02;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class try_finally {
 	static String firstLineOfFile(String path) throws IOException {
@@ -11,6 +15,22 @@ public class try_finally {
 			return br.readLine();
 		} finally {
 			br.close();
+		}
+	}
+	static void copy(String src, String dst) throws IOException{
+		InputStream in = new FileInputStream(src);
+		try {
+			OutputStream out = new FileOutputStream(dst);
+			try {
+				byte [] buf = new byte[BUFFER_SIZE];
+				int n;
+				while ((n = in.read(buf)) >= 0)
+					out.write(buf, 0, n);
+			} finally {
+				out.close();
+			}
+		} finally {
+			in.close();
 		}
 	}
 }
