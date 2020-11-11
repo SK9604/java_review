83064063569f662a427f85b25be4f1cd68145279
SK9604 <tjsrud428@gmail.com>
2020-11-11 18:04:40 +0900

    try-with-resources

--- src/ch02/try_finally.java
+++ src/ch02/try_finally.java
@@ -10,27 +10,17 @@ import java.io.OutputStream;
 
 public class try_finally {
 	static String firstLineOfFile(String path) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		try {
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			return br.readLine();
-		} finally {
-			br.close();
 		}
 	}
 	static void copy(String src, String dst) throws IOException{
-		InputStream in = new FileInputStream(src);
-		try {
-			OutputStream out = new FileOutputStream(dst);
-			try {
-				byte [] buf = new byte[BUFFER_SIZE];
-				int n;
-				while ((n = in.read(buf)) >= 0)
-					out.write(buf, 0, n);
-			} finally {
-				out.close();
-			}
-		} finally {
-			in.close();
+		try (InputStream in = new FileInputStream(src);
+			OutputStream out = new FileOutputStream(dst)) {
+			byte [] buf = new byte[BUFFER_SIZE];
+			int n;
+			while ((n = in.read(buf)) >= 0)
+				out.write(buf, 0, n);
 		}
 	}
 }
